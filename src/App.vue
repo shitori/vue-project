af53ec3757577e0bf706e116076e321a202355e6
shitori <antonyfourchualt@gmail.com>
2023-04-05 14:07:08 +0200

    reformat code with prettier + eslint

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,62 @@
 <script>
-import FormUpdateSquashTests from './components/FormUpdateSquashTests.vue'
-import LoadingScreenUpdateSquashTests from './components/LoadingScreenUpdateSquashTests.vue'
-import ResultUpdateSquashTests from './components/ResultUpdateSquashTests.vue'
-import InfoSquashTests from './components/InfoSquashTests.vue'
-import SocketioService from './services/socket'
-import { storeVisibility as store } from './services/store'
+import FormUpdateSquashTests from "./components/FormUpdateSquashTests.vue";
+import LoadingScreenUpdateSquashTests from "./components/LoadingScreenUpdateSquashTests.vue";
+import ResultUpdateSquashTests from "./components/ResultUpdateSquashTests.vue";
+import InfoSquashTests from "./components/InfoSquashTests.vue";
+import SocketioService from "./services/socket";
+import { storeVisibility as store } from "./services/store";
 export default {
-  data() {
-    return {
-      store
-    }
-
-  },
   components: {
     FormUpdateSquashTests,
     LoadingScreenUpdateSquashTests,
     ResultUpdateSquashTests,
     InfoSquashTests,
   },
+  data() {
+    return {
+      store,
+    };
+  },
   created() {
-    SocketioService.setupSocketConnection()
+    SocketioService.setupSocketConnection();
   },
   beforeUnmount() {
-    SocketioService.disconnect()
+    SocketioService.disconnect();
   },
   mounted() {
-    document.body.classList.add('container')
-    document.body.classList.add('is-fluid')
-  }
-}
+    document.body.classList.add("container");
+    document.body.classList.add("is-fluid");
+  },
+};
 </script>
 
 <template>
-  <header>
-
-  </header>
+  <header></header>
 
   <main class="container mt-6">
     <div class="has-text-centered">
       <p class="title has-text-black">FCC passerel</p>
 
-      <a class="button is-primary is-outlined is-large m-1"
+      <a
+        class="button is-primary is-outlined is-large m-1"
         href="http://172.27.0.229:8080/jenkins2/view/FCC%20GUI%20-%20Tests%20Robot%20Framework/"
-        target="_blank">Jenkins</a>
-      <a class="button is-primary is-outlined is-large m-1"
+        target="_blank"
+        >Jenkins</a
+      >
+      <a
+        class="button is-primary is-outlined is-large m-1"
         href="https://testmanagement.factory.orange-business.com/squash/campaign-workspace/campaign-folder/13627/content"
-        target="_blank">Squash</a>
+        target="_blank"
+        >Squash</a
+      >
     </div>
 
     <FormUpdateSquashTests :class="{ 'is-hidden': !store.formUST }" />
 
-    <LoadingScreenUpdateSquashTests :class="{ 'is-hidden': !store.loadUST }"/>
+    <LoadingScreenUpdateSquashTests :class="{ 'is-hidden': !store.loadUST }" />
 
-    <ResultUpdateSquashTests :class="{ 'is-hidden': !store.resultUST }"/>
+    <ResultUpdateSquashTests :class="{ 'is-hidden': !store.resultUST }" />
 
-    <InfoSquashTests/>
-
+    <InfoSquashTests />
   </main>
 </template>
--- src/components/ResultUpdateSquashTests.vue
+++ src/components/ResultUpdateSquashTests.vue
@@ -1,60 +1,79 @@
 <script>
-import { storeResult as store, storeVisibility as componentsVisibility } from '../services/store'
-import { FAILURE, SUCCESS } from '../interfaces/interfaceStutusTestSquash'
+import {
+  storeResult as store,
+  storeVisibility as componentsVisibility,
+} from "../services/store";
+import { FAILURE, SUCCESS } from "../interfaces/interfaceStutusTestSquash";
 export default {
-    data() {
-        return {
-            store,
-            typeStatusSquash: { FAILURE, SUCCESS }
-        }
-    },
-    methods: {
-        removeHTMLtagsAndShorter(html) {
-            const div = document.createElement("div");
-            div.innerHTML = html;
-            let text = div.textContent || div.innerText || "";
+  data() {
+    return {
+      store,
+      typeStatusSquash: { FAILURE, SUCCESS },
+    };
+  },
+  methods: {
+    removeHTMLtagsAndShorter(html) {
+      const div = document.createElement("div");
+      div.innerHTML = html;
+      let text = div.textContent || div.innerText || "";
 
-            return text.substring(0, 174)
-        },
-        resetComponentVisibility() {
-            componentsVisibility.setFormUST(true)
-            componentsVisibility.setloadUST(false)
-            componentsVisibility.setResultUST(false)
-        }
-    }
-}
+      return text.substring(0, 174);
+    },
+    resetComponentVisibility() {
+      componentsVisibility.setFormUST(true);
+      componentsVisibility.setloadUST(false);
+      componentsVisibility.setResultUST(false);
+    },
+  },
+};
 </script>
 
 <template>
-    <div class="box mt-6 has-text-centered">
+  <div class="box mt-6 has-text-centered">
+    <p class="subtitle m-1">{{ store.message }}</p>
+    <button
+      class="button is-info is-outlined is-medium m-1"
+      @click="resetComponentVisibility()"
+    >
+      Nouvelle itération
+    </button>
+    <pre class="m-5 has-text-left">{{ store.moreInfo }}</pre>
 
-        <p class="subtitle m-1 ">{{ store.message }}</p>
-        <button class="button is-info is-outlined is-medium m-1" @click="resetComponentVisibility()">Nouvelle itération
-        </button>
-        <pre class="m-5 has-text-left">{{ store.moreInfo }}</pre>
-
-        <div class="columns is-multiline has-text-left is-centered ">
-            <div class="card m-1 column is-one-quarter " style="width:  20rem;" v-for="result in store.result" :class="{
-                'has-background-success': (result.status === typeStatusSquash.SUCCESS),
-                'has-background-danger': (result.status === typeStatusSquash.FAILURE)
-            }">
-                <a target="_blank" class="is-link"
-                    :href="'https://testmanagement.factory.orange-business.com/squash/test-case-workspace/test-case/' + result.realId + '/content'">
-                    <div class="card-content">
-                        <div class="media">
-                            <div class="media-content">
-                                <p class="title is-4">{{ result.testName }}</p>
-                                <p class="subtitle is-6">{{ result.status }}</p>
-                            </div>
-                        </div>
-
-                        <div class="content">
-                            <p class="has-text-dark">{{ removeHTMLtagsAndShorter(result.extraInfo) }}</p>
-                        </div>
-
-                    </div>
-                </a>
+    <div class="columns is-multiline has-text-left is-centered">
+      <div
+        v-for="result in store.result"
+        class="card m-1 column is-one-quarter"
+        style="width: 20rem"
+        :class="{
+          'has-background-success': result.status === typeStatusSquash.SUCCESS,
+          'has-background-danger': result.status === typeStatusSquash.FAILURE,
+        }"
+      >
+        <a
+          target="_blank"
+          class="is-link"
+          :href="
+            'https://testmanagement.factory.orange-business.com/squash/test-case-workspace/test-case/' +
+            result.realId +
+            '/content'
+          "
+        >
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">{{ result.testName }}</p>
+                <p class="subtitle is-6">{{ result.status }}</p>
+              </div>
             </div>
-        </div>
+
+            <div class="content">
+              <p class="has-text-dark">
+                {{ removeHTMLtagsAndShorter(result.extraInfo) }}
+              </p>
+            </div>
+          </div>
+        </a>
+      </div>
     </div>
+  </div>
 </template>
